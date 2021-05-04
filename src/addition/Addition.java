953366c00b805d7bda33069c186278ab8bf8d6a7
amrin1987 <amrin01@gmail.com>
2021-05-04 09:51:10 -0400

    commit by WorkSpace2

--- src/addition/Addition.java
+++ src/addition/Addition.java
@@ -4,13 +4,22 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Addition obj=new Addition();
-		obj.additionMethod(5,6);
+		Addition obj = new Addition();
+		obj.additionMethod(5, 6);
+		obj.additionMethod(9, 0, 8);
 	}
-public  int additionMethod(int a, int b) {
-	 
-	     int c=a+b;
-	     System.out.println("result is "+c);
+
+	public int additionMethod(int a, int b) {
+
+		int c = a + b;
+		System.out.println("result is " + c);
 		return c;
-}
+	}
+
+	public int additionMethod(int x, int y, int z) {
+		int total = x + y + z;
+		System.out.println("result is " + total);
+		return total;
+
+	}
 }
